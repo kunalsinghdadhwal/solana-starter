d046917f64f9a5b0dce29b820676e188fbc0bf23
Kunal Singh Dadhwal <kunalsinghdadhwal@gmail.com>
2026-02-04 23:46:27 +0530

    readme, metadata, transfer
    Signed-off-by: Kunal Singh Dadhwal <kunalsinghdadhwal@gmail.com>

--- ts/README.md
+++ ts/README.md
@@ -22,3 +22,30 @@ kunal@Crusader:~/solana-starter/ts$ pnpm run spl_mint
 Your ata is: 8b4Xov8NwciDKLdu43hSUJu9x5uBEzrEJoyJEvAMxeKv
 Your mint txid: 637kMw9oLzXUG2Wcqp6FmMnhcs5RKR7GjczBTLUvNw6keunu39FLRCihWqzXJCJxi85oZWkBsyXsqAenPZYYeBaK
 ```
+
+### Task2: SPL Metadata
+
+#### Metadata
+```bash
+kunal@Crusader:~/solana-starter/ts$ pnpm run spl_metadata
+
+> turbin3-solana@1.0.0 spl_metadata /home/kunal/solana-starter/ts
+> ts-node ./cluster1/spl_metadata.ts
+
+2W97mWBFKK4oDRa6jLqcKXcKg5ae5dTBvYjxMV1EdGzWGvZmUZaSxApCuPngapGawVdQqpDmcJ1Wf2JdGtUoEzNs
+```
+
+### Task3: SPL Transfer
+
+#### Transfer
+```bash
+kunal@Crusader:~/solana-starter/ts$ pnpm run spl_transfer
+
+> turbin3-solana@1.0.0 spl_transfer /home/kunal/solana-starter/ts
+> ts-node ./cluster1/spl_transfer.ts
+
+bigint: Failed to load bindings, pure JS will be used (try npm run rebuild?)
+From ATA: 8b4Xov8NwciDKLdu43hSUJu9x5uBEzrEJoyJEvAMxeKv
+To ATA: 49LDtKAruHsuj8boa7hUixCCYXPQYhxKoMXbzTQx31B5
+Your transfer txid: 2ggkunMt5ihnxjGLUzK56n8e5CL8VZUQ3Nd4gdTEtteN4S6vxLjyPrN5bXxGvjUadZrxWdk4PeJ66iSKpxJBomYV
+```
--- ts/cluster1/spl_transfer.ts
+++ ts/cluster1/spl_transfer.ts
@@ -10,18 +10,41 @@ const commitment: Commitment = "confirmed";
 const connection = new Connection("https://api.devnet.solana.com", commitment);
 
 // Mint address
-const mint = new PublicKey("<mint address>");
+const mint = new PublicKey("F2a2mKwMhhZnwEU2p6nJUy3n19qVZUnYyn86Sp87o1Y");
 
 // Recipient address
-const to = new PublicKey("<receiver address>");
+const to = new PublicKey("KUNALdCpU47T1tEaoTXq1VAqcWfkPefUqGXTSW1NjmE");
 
 (async () => {
     try {
         // Get the token account of the fromWallet address, and if it does not exist, create it
+        const from_ata = await getOrCreateAssociatedTokenAccount(
+            connection,
+            keypair,
+            mint,
+            keypair.publicKey
+        );
+        console.log(`From ATA: ${from_ata.address.toBase58()}`);
 
         // Get the token account of the toWallet address, and if it does not exist, create it
+        const to_ata = await getOrCreateAssociatedTokenAccount(
+            connection,
+            keypair,
+            mint,
+            to
+        );
+        console.log(`To ATA: ${to_ata.address.toBase58()}`);
 
         // Transfer the new token to the "toTokenAccount" we just created
+        const tx = await transfer(
+            connection,
+            keypair,
+            from_ata.address,
+            to_ata.address,
+            keypair,
+            1_000_000n
+        );
+        console.log(`Your transfer txid: ${tx}`);
     } catch(e) {
         console.error(`Oops, something went wrong: ${e}`)
     }
